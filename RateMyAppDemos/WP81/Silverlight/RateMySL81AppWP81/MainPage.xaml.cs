fd4615e4d132c21817e416b2a25199dae06bac49
Lucian Tomuta <lucian.tomuta@gmail.com>
2014-07-13 21:53:06 +0300

    Silverlight 8.1 support
    Well, it seems to do the job, but more testing will be needed. New
    solution created as VS2013 does not support the WP7 projects contained
    on the original one. SL8.1 support consists basically of just the
    upgraded .csproj file

---
+++ RateMyAppDemos/WP81/Silverlight/RateMySL81AppWP81/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿/**
+ * Copyright (c) 2013-2014 Microsoft Mobile. All rights reserved.
+ *
+ * Nokia, Nokia Connecting People, Nokia Developer, and HERE are trademarks
+ * and/or registered trademarks of Nokia Corporation. Other product and company
+ * names mentioned herein may be trademarks or trade names of their respective
+ * owners.
+ *
+ * See the license text file delivered with this project for more information.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using RateMyXamlAppWP8.Resources;
+
+namespace RateMyXamlAppWP8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BuildApplicationBar();
+
+            FeedbackOverlay.VisibilityChanged += FeedbackOverlay_VisibilityChanged;
+
+#if DEBUG
+            // Read the internal state of the Rate My App control
+            DataContext = RateMyApp.Helpers.FeedbackHelper.Default;
+#endif
+        }
+
+        void FeedbackOverlay_VisibilityChanged(object sender, EventArgs e)
+        {
+            ApplicationBar.IsVisible = (FeedbackOverlay.Visibility != Visibility.Visible);
+        }
+
+        private void BuildApplicationBar()
+        {
+            // Set the page's ApplicationBar to a new instance of ApplicationBar
+            ApplicationBar = new ApplicationBar();
+            ApplicationBar.Mode = ApplicationBarMode.Minimized;
+
+            // Create reset menu item
+            ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemResetText);
+            appBarMenuItem.Click += new EventHandler(Reset_Click);
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+            // Create reset menu item
+            appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemAboutText);
+            appBarMenuItem.Click += new EventHandler(About_Click);
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            FeedbackOverlay.Reset();
+        }
+
+        private void About_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/AboutPage.xaml", UriKind.Relative));
+        }
+    }
+}
